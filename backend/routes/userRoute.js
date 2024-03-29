9efdd9ffbfc29006892f36d9d6c1cc0bfb627251
NgxLama <nguyenduclam1911@gmail.com>
2024-03-29 18:40:46 +0700

    thêm multer, thêm api đăng xuất, updateProfile

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,12 +2,18 @@ import express from 'express';
 
 import {
   register,
-  login
+  login,
+  logout,
+  updateProfile
 } from '../controllers/userController.js';
+import multer from 'multer';
 
 const router = express.Router();
+const upload = multer();
 
-router.post('/register', register);
-router.post('/login', login)
+router.post('/register', upload.none(), register);
+router.post('/login', upload.none(), login);
+router.post('/logout', upload.none(), logout);
+router.post('/updateProfile/:idUser', upload.single('cover'), updateProfile);
 
 export default router;
