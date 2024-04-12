39def8ef701ad5612e0756b7b394c007012946e1
Nguyễn Hữu Lâm <48357664+huulamthcsyl@users.noreply.github.com>
2024-04-12 21:17:50 +0700

    Add web icon and name

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,7 +4,8 @@ import notification from '../assets/icons/notification.png'
 import messenger from '../assets/icons/messenger.png'
 import friends from '../assets/icons/friends.png'
 import user from '../assets/icons/user.png'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import logo from '../assets/icons/logo.png'
 
 /**
  * Represents the navigation bar component.
@@ -23,17 +24,22 @@ export default function NavBar() {
     <Container className='p-2 border-bottom sticky-top' fluid style={{ height: '10%', backgroundColor: '#fff' }}>
       <Row>
         <Col>
-          <Form>
-            <Form.Control className='rounded-pill' type="text" placeholder="Tìm kiếm" style={{width: '30%', height: '50px'}} />
-          </Form>
+          <Container className='p-0 d-flex'>
+            <Link className='me-2 mt-1' to={'/'}>
+              <Image src={logo} style={{ width: '40px' }} />
+            </Link>
+            <Form>
+              <Form.Control className='rounded-pill' type="text" placeholder="Tìm kiếm" style={{ height: '50px' }} />
+            </Form>
+          </Container>
         </Col>
         <Col className='d-flex flex-row-reverse'>
-          <NavDropdown title={<Image src={user} className='border p-1 ms-4' roundedCircle/>} id="basic-nav-dropdown">
+          <NavDropdown title={<Image src={user} className='border p-1 ms-4' roundedCircle />} id="basic-nav-dropdown">
             <NavDropdown.Item onClick={handleLogout}>Đăng xuất</NavDropdown.Item>
           </NavDropdown>
-          <Image className='border p-1 ms-4' src={notification} roundedCircle/>
-          <Image className='border p-1 ms-4' src={messenger} roundedCircle/>
-          <Image className='border p-1 ms-4' src={friends} roundedCircle/>
+          <Image className='border p-1 ms-4' src={notification} roundedCircle />
+          <Image className='border p-1 ms-4' src={messenger} roundedCircle />
+          <Image className='border p-1 ms-4' src={friends} roundedCircle />
         </Col>
       </Row>
     </Container>
